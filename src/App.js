2282a5ddf14e4a84ad1ce3f8583eeafb828e33e1
abhishek <abhishekritu810219@gmail.com>
2022-12-08 20:06:35 +0530

    from

--- src/App.js
+++ src/App.js
@@ -1,99 +1,81 @@
-import React, { useReducer, useState, useRef } from 'react'
+import React, { useReducer, useState } from 'react'
 import './App.css'
 
-const initialState = {
-  todos: [],
-  todo: "",
-  completed: false,
-}
+const initialState = [{ id: Math.random(), name: "abhishek", email: "abhishek@gmail.com", completed: false },]
 
 const todoReducer = (state, action) => {
   switch (action.type) {
-    case "ADD":
-      return {
-        ...state, todos: [...state.todos, action.payload]
-      }
-    case "SET":
-      return {
-        ...state, todo: action.payload
-      }
-    case "DELETE":
-      const newTodo = [...state.todos].filter((item, index) => index !== action.payload);
-      return {
-        ...state, todos: newTodo
-      }
-    case "UPDATE":
-      const newJobs2 = [...state.todos];
-      newJobs2[action.payload.index] = action.payload.todo;
-      return { ...state, todos: newJobs2 };
-
-    // case "TOGGLE": {
-    //   todos: state.todos.map((item, index) =>
-    //     state.todos.index === action.payload.index ? { ...item, completed: !item.completed } : item)
-    // }
-    //   return { ...state.todos }
-
+    case "add":
+      return [...state, action.payload]
+    case "delete":
+      return state.filter((contact) => {
+        return contact.id !== action.payload.id;
+      })
+    case "toggle":
+      return state.map((contact) => {
+        return contact.id === action.payload.id ? { ...contact, completed: !contact.completed } : contact
+      })
     default:
       return state;
   }
+
 }
 
 const App = () => {
-  const ref = useRef();
-  const [state, dispatch] = useReducer(todoReducer, initialState);
-  const { todo, todos, completed } = state;
-  const [currentIndex, setIndex] = useState(-1);
+  const [state, dispatch] = useReducer(todoReducer, initialState)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (todo) {
-      if (currentIndex === -1) {
-        // const newTodo = {todo, complete: false,}
-        dispatch({ type: "ADD", payload: todo });
-      } else {
-        dispatch({ type: "UPDATE", payload: { todo, index: currentIndex } });
-
+    if (name && email) {
+      const contact = {
+        id: Math.random(),
+        name: name,
+        email: email,
+        completed: false,
       }
+      console.log(contact);
+      dispatch({ type: "add", payload: contact })
     }
-    dispatch({ type: "SET", payload: "" });
-    ref.current.focus();
-    setIndex(-1);
-  };
-
+    setName('')
+    setEmail('')
+  }
 
   return (
     <>
-      <div className='App'>
-        <h4>Todo-App</h4>
-        <h4>{todos.length}</h4>
-        <form onSubmit={handleSubmit}>
-          <input ref={ref} value={todo} onChange={(e) => { dispatch({ type: "SET", payload: e.target.value }) }} />
-          {/* <button onClick={handleSubmit}>submit</button> */}
-        </form>
+      <div className="App">
+        <div>
+          <form>
+            <input placeholder='name' value={name} onChange={(e) => setName(e.target.value)} />
+            <input placeholder='name' value={email} onChange={(e) => setEmail(e.target.value)} />
+            <button onClick={handleSubmit}>submit</button>
+          </form>
+        </div>
 
         <div>
-          {
-            todos.map((item, index) => (
-              <div key={index}>
-                <ul style={{ textDecoration: item ? "line-through" : "" }}
-                  onClick={() => {
-                    setIndex(index);
-                    dispatch({ type: "SET", payload: item });
-                  }}>{item}</ul>
-                <button onClick={() => dispatch({ type: "DELETE", payload: index })}>delete</button>
-                <button onClick={() => dispatch({ type: "TOGGLE", payload: index })}>Toggle</button>
-              </div>
-            ))
-          }
+          {state.map(contact => {
+            return (
+              <li style={{ listStyle: "none", paddingLeft: "0" }} key={contact.id}>
+                <span style={{ textDecoration: contact.completed ? "line-through" : "" }}>{contact.name}</span>
+                <br />
+                <span style={{ textDecoration: contact.completed ? "line-through" : "" }}>{contact.email}</span>
+                <div>
+                  <button onClick={() => dispatch({ type: "delete", payload: { id: contact.id } })}>delete</button>
+                  <button onClick={() => dispatch({ type: "toggle", payload: { id: contact.id } })}>toggle</button>
+                  {/* <button onClick={() => dispatch({ type: "SET", payload: { name: contact.name, email: contact.email } })}>Edit</button> */}
+                </div>
+              </li>
+            )
+          })}
         </div>
       </div>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+
     </>
   )
 }
 
 export default App
-
-
-
-
-
--- src/notes.js
+++ src/notes.js
@@ -153,3 +153,104 @@ export default function App() {
     </div>
   );
 }
+
+//////////
+import React, { useReducer, useState, useRef } from 'react'
+import './App.css'
+
+const initialState = {
+  todos: [],
+  todo: "",
+  completed: false,
+}
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      return {
+        ...state, todos: [...state.todos, action.payload]
+      }
+    case "SET":
+      return {
+        ...state, todo: action.payload
+      }
+    case "DELETE":
+      const newTodo = [...state.todos].filter((item, index) => index !== action.payload);
+      return {
+        ...state, todos: newTodo
+      }
+    case "UPDATE":
+      const newJobs2 = [...state.todos];
+      newJobs2[action.payload.index] = action.payload.todo;
+      return { ...state, todos: newJobs2 };
+
+    // case "TOGGLE": {
+    //   todos: state.todos.map((item, index) =>
+    //     state.todos.index === action.payload.index ? { ...item, completed: !item.completed } : item)
+    // }
+    //   return { ...state.todos }
+
+    default:
+      return state;
+  }
+}
+
+const App = () => {
+  const ref = useRef();
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const { todo, todos, completed } = state;
+  const [currentIndex, setIndex] = useState(-1);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todo) {
+      if (currentIndex === -1) {
+        // const newTodo = {todo, complete: false,}
+        dispatch({ type: "ADD", payload: todo });
+      } else {
+        dispatch({ type: "UPDATE", payload: { todo, index: currentIndex } });
+
+      }
+    }
+    dispatch({ type: "SET", payload: "" });
+    ref.current.focus();
+    setIndex(-1);
+  };
+
+
+  return (
+    <>
+      <div className='App'>
+        <h4>Todo-App</h4>
+        <h4>{todos.length}</h4>
+        <form onSubmit={handleSubmit}>
+          <input ref={ref} value={todo} onChange={(e) => { dispatch({ type: "SET", payload: e.target.value }) }} />
+          {/* <button onClick={handleSubmit}>submit</button> */}
+        </form>
+
+        <div>
+          {
+            todos.map((item, index) => (
+              <div key={index}>
+                <ul style={{ textDecoration: item ? "line-through" : "" }}
+                  onClick={() => {
+                    setIndex(index);
+                    dispatch({ type: "SET", payload: item });
+                  }}>{item}</ul>
+                <button onClick={() => dispatch({ type: "DELETE", payload: index })}>delete</button>
+                <button onClick={() => dispatch({ type: "TOGGLE", payload: index })}>Toggle</button>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
